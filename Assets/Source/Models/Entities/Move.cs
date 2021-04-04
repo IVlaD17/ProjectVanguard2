3ff9ddd0a2fc0572a6a76e44fb1151627bf2e84d
Vlad Ionica <mihaiionica10@gmail.com>
2021-04-04 14:49:58 +0100

    Created a new Move class.

---
+++ Assets/Source/Models/Entities/Move.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Source.Models.Entities
+{
+    public class Move
+    {
+        public string MyName { get; private set; }
+        public int DeltaRow { get; private set; }
+        public int DeltaCol { get; private set; }
+
+        public Move(string myName, int deltaRow, int deltaCol)
+        {
+            MyName = myName;
+            DeltaRow = deltaRow;
+            DeltaCol = deltaCol;
+        }
+    }
+}
